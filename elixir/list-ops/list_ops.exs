0001e2f13705116dc4000746b82ea2c3b3cec06e
May Pongpitpitak <me@mayppong.com>
2015-10-18 18:22:20 -0700

    Reduce and append done.

---
+++ elixir/list-ops/list_ops.exs
@@ -0,0 +1,54 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List) in your
+  # implementation. The point of this exercise is to create these basic functions
+  # yourself.
+  #
+  # Note that `++` is a function from an external module (Kernel, which is
+  # automatically imported) and so shouldn't be used either.
+
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+    
+  end
+
+  @spec reverse(list) :: list
+  def reverse(l) do
+
+  end
+  def reverse(l, output) do
+    [output | tl(l)]
+  end
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+
+  end
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
+  def reduce(l, acc, f) do
+    case l do
+      [] ->
+        acc
+      [item] ->
+        f.(item, acc)
+      [item | less] ->
+        reduce(less, f.(item,acc), f)
+    end
+  end
+
+  @spec append(list, list) :: list
+  def append(a, b) do
+    a ++ b
+  end
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
+
+  end
+end
