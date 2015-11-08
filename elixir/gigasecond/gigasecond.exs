cee53dbf3d3f1bf989995eb16bda42b7bca6b615
May Pongpitpitak <me@mayppong.com>
2015-11-08 12:48:52 -0800

    Calculate by days instead of second
    Instead of converting all the way to seconds and back, we could just do
    it by number of days, equivalent of 1 billion seconds.
    
    Since we don't need seconds precision level anyway, this shouldn't
    matter.

--- elixir/gigasecond/gigasecond.exs
+++ elixir/gigasecond/gigasecond.exs
@@ -5,19 +5,10 @@ defmodule Gigasecond do
   @spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
 
   def from({year, month, day}) do
-    {date, time} = :calendar.date_to_gregorian_days(year, month, day)
-    |> day_to_second
-    |> add_billion_seconds
-    |> :calendar.gregorian_seconds_to_datetime
+    {days_since, _} = :calendar.seconds_to_daystime(1_000_000_000)
 
-    date
-  end
-
-  def day_to_second(day) do
-    day * 86400
-  end
-
-  def add_billion_seconds(second) do
-    second + 1_000_000_000
+    :calendar.date_to_gregorian_days(year, month, day)
+    |> + days_since
+    |> :calendar.gregorian_days_to_date
   end
 end
