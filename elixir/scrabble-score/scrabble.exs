c6ca6345a45d3e6f2398d185fed9b4e6ea4b9ea5
May Pongpitpitak <me@mayppong.com>
2015-11-08 13:53:40 -0800

    Finished scrabble-score!!

---
+++ elixir/scrabble-score/scrabble.exs
@@ -0,0 +1,26 @@
+defmodule Scrabble do
+
+  @letter_score %{
+    "a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1, "l" => 1, "n" => 1, "r" => 1, "s" => 1, "t" => 1,
+    "d" => 2, "g" => 2,
+    "b" => 3, "c" => 3, "m" => 3, "p" => 3,
+    "f" => 4, "h" => 4, "v" => 4, "w" => 4, "y" => 4,
+    "k" => 5,
+    "j" => 8, "x" => 8,
+    "q" => 10, "z" => 10
+  }
+
+  @doc """
+  Calculate the scrabble score for the word.
+  """
+  @spec score(String.t) :: non_neg_integer
+  def score(word) do
+    String.replace(word, ~r/[^a-z]/i, "")
+      |> String.strip
+      |> String.downcase
+      |> String.split("", trim: true)
+      |> Enum.reduce(0, fn(letter, acc) ->
+          acc + @letter_score[letter]
+        end)
+  end
+end
