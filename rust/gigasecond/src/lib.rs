4e32f7590bc34aa2f8752f4df546e649a35ee963
May Pongpitpitak <me@mayppong.com>
2020-06-14 22:13:13 -0700

    Complete gigasecond exercise for Rust

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,6 @@
-extern crate chrono;
 use chrono::{DateTime, Utc, Duration};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(10_i64.pow(9))
+    start + Duration::seconds(1_000_000_000)
 }
